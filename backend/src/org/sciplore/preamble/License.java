8ae63cec6f7c519450424275d3a260852b26d62d
physikerwelt <wiki@physikerwelt.de>
2024-09-06 07:51:55 +0200

    Update outdated links pointing to isg.uni.kn

--- backend/src/org/sciplore/preamble/License.java
+++ backend/src/org/sciplore/preamble/License.java
@@ -3,8 +3,8 @@ import java.lang.annotation.*;
 
 @Retention(RetentionPolicy.RUNTIME)
 public @interface License {
-	String author () default "AG Gipp (www.isg.uni.kn)";
-	String license() default "Copyright (c) 2015, Information Science Group, University of Konstanz (http://www.isg.uni-konstanz.de)"
+	String author () default "Gipplab (www.gipplab.org)";
+	String license() default "Copyright (c) 2015, Chair for Scientific Information Analytics, University of Göttingen (https://gipplab.org/)"
 	+"\nBela Gipp (http://gipp.com/contact), Norman Meuschke (www.meuschke.org), Mario Lipinski (www.lipinski.tk) and others."
 	+"\nFor publications related to this work see www.isg.uni-konstanz.de/publications/pub."
 	+"\npublished under MIT License";
